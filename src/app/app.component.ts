3f73abee4cc492de1d7aed09f3459838828e2aec
dresdenevents <mehrzusammensein@gmail.com>
2023-05-14 18:24:47 +0200

    Add more content & palette

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
+import {ContentService} from "./content.service";
+import {EMPTY, Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'BLE-Angular';
+export class AppComponent implements OnInit {
+
+  footerContent$: Observable<any> = EMPTY;
+  menuContent$: Observable<any> = EMPTY;
+
+  constructor(private contentService: ContentService, private el: ElementRef) {}
+
+  ngOnInit(): void {
+    this.contentService.loadContentDataByName('home/menu.json')
+    this.menuContent$ = this.contentService.getContentById('menu');
+
+    this.contentService.loadContentDataByName('home/footer.json')
+    this.footerContent$ = this.contentService.getContentById('footer');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {RouterModule, Routes} from "@angular/router";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HomeComponent} from './home/home.component';
+import {SharedModule} from "./shared/shared.module";
+import {AboutComponent} from './about/about.component';
 
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [
+    HttpClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
